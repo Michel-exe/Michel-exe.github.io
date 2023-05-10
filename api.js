183683db9389234a99d612a7f9b22dfaefe6bacf
Develop <71541698+Michel-exe@users.noreply.github.com>
2023-05-10 13:59:20 -0600

    Create api.js

---
+++ api.js
@@ -0,0 +1,133 @@
+let establecimientos = [
+   {
+      idCat: 'idCat1',
+      category: 'Hoteleria',
+      data: [
+         {
+            id: "estab1",
+            name: "mapa de nanacamilpa",
+            refe: "Nanacamilpa Tlaxcala",
+            url: "https://www.google.com/maps/d/edit?mid=10p5gr5ryGSJ1XzxRrjJWSM32x_V36rI&usp=sharing",
+            loc: {
+               latitude: 19.493037,
+               longitude: -98.535733
+            },
+         }
+      ]
+   }, {
+      idCat: 'idCat9',
+      category: 'Comida Mexicana',
+      data: [
+         {
+            id: "estab2",
+            name: "Michel",
+            refe: "Nanacamilpa Tlaxcala",
+            url: "https://www.google.com/maps/d/u/0/edit?mid=1ZHaEpEjoJFMFWQZE167uD0x-aj1Zkl4&usp=sharing",
+            loc: {
+               latitude: 19.493037,
+               longitude: -98.535733
+            },
+         },
+         {
+            id: "estab3",
+            name: "casa de historia",
+            refe: "Lorem impus dolot itsum mit Lorem impus dolot itsum mit Lorem impus dolot itsum mit Lorem impus dolot itsum mit",
+            url: "https://www.google.com/maps/d/edit?mid=1c1lL348BzioD_3Vw_n2BxewLFVGh4x0&usp=sharing",
+            loc: {
+               latitude: 19.487706104427147,
+               longitude: -98.53741132866453
+            },
+         }]
+   }, {
+      idCat: 'idCat2',
+      category: 'Restaurant',
+      data: [{
+         id: "estab4",
+         name: "san cayetano ",
+         refe: "Lorem impus dolot itsum mit",
+         url: "https://www.google.com/maps/d/edit?mid=1p9a6HgT0wbeOgWNm3RnFmBzgFrlTi-k&usp=sharing",
+         loc: {
+            latitude: 19.50904,
+            longitude: -98.53636
+         },
+      },
+      {
+         id: "estab5",
+         name: "Vikingos",
+         refe: "Lorem impus dolot itsum mit",
+         url: "https://www.google.com/maps/d/edit?mid=1DgX54cXRhvAu0HUAZNhHRqn7yd81OuU&usp=sharing",
+         loc: {
+            latitude: 19.49277,
+            longitude: -98.53524
+         },
+      },
+      {
+         id: "estab6",
+         name: "Hotele Milenio",
+         refe: "Lorem impus dolot itsum mit",
+         url: "https://www.google.com/maps/d/edit?mid=1eoFvGy2vKli9VBCkfm-c_AfmOlGObGo&usp=sharing",
+         loc: {
+            latitude: 19.49176,
+            longitude: -98.54279
+         },
+      }]
+   }, {
+      idCat: 'idCat3',
+      category: 'Bares',
+      can: {},
+      data: [
+         {
+            id: 5,
+            name: "El Mareo",
+            refe: "Lorem impus dolot itsum mit",
+            url: "https://www.google.com/maps/d/edit?mid=18Lrw-mfIzPg0n_lZn8APOHkWJT0wsdw&usp=sharing",
+            loc: {
+               latitude: 19.49597,
+               longitude: -98.53468
+            },
+         },
+      ]
+   }, {
+      idCat: 'idCat4',
+      category: 'Demas',
+      can: {},
+      data: [
+         {
+            id: 8,
+            name: "Tepozontitla",
+            refe: "Lorem impus dolot itsum mit",
+            url: "https://www.google.com/maps/d/edit?mid=1gvxNEba3M-w7QrqC6FqGbZdPjdKqIio&usp=sharing",
+            loc: {
+               latitude: 19.51133,
+               longitude: -98.53118
+            },
+         }
+      ]
+   }, {
+      idCat: 'idCat5',
+      category: 'Pulqueria',
+      can: {},
+      data: [
+         {
+            id: 6,
+            name: "Mil Amores",
+            refe: "Lorem impus dolot itsum mit",
+            url: "https://www.google.com/maps/d/edit?mid=1jmE7sOZRebHS2i4cBQ81aCDZfUA9IhM&usp=sharing",
+            loc: {
+               latitude: 19.49497,
+               longitude: -98.53534
+            },
+         },
+         {
+            id: 7,
+            name: "Mama Panchita",
+            refe: "Lorem impus dolot itsum mit",
+            url: "https://www.google.com/maps/d/edit?mid=11tJFOxZbeMO0PlRmSRz6XaVi0ybA8YE&usp=sharing",
+            loc: {
+               latitude: 19.49361,
+               longitude: -98.53506
+            },
+         },
+      ]
+   }
+]
