05e39f8484bf1df2ea60069b62b34937ec581588
Develop <71541698+Michel-exe@users.noreply.github.com>
2023-05-17 20:07:09 -0600

    Update api.js

--- api.js
+++ api.js
@@ -11,85 +11,85 @@
             "loc": {
                "latitude": 19.493037,
                "longitude": -98.535733
-            }
-         }
-      ],
-      "info": [
-         {
-            "idEstab": "estab1",
-            "name": "Hotel",
-            "desing": 1,
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed ex tincidunt, condimentum .",
-            "details": [
+            },
+            "info": [
                {
-                  "ide": "hotel1",
-                  "tipo": "tipo_Habitacion",
-                  "costo": "100 MXN"
-               },
-               {
-                  "ide": "hotel2",
-                  "tipo": "tipo_Habitacion",
-                  "costo": "100 MXN"
-               },
-               {
-                  "ide": "hotel3",
-                  "tipo": "tipo_Habitacion",
-                  "costo": "100 MXN"
-               },
-               {
-                  "ide": "hotel4",
-                  "tipo": "tipo_Habitacion",
-                  "costo": "100 MXN"
-               },
-               {
-                  "ide": "hotel5",
-                  "tipo": "tipo_Habitacion",
-                  "costo": "100 MXN"
-               }
-            ],
-            "source": [
-               {
-                  "idima": "img1",
-                  "img": "https://resizer.otstatic.com/v2/photos/wide-huge/2/47967436.jpg"
-               },
-               {
-                  "idima": "img2",
-                  "img": "https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_640.jpg"
-               },
-               {
-                  "idima": "img3",
-                  "img": "https://cdn.pixabay.com/photo/2015/11/19/10/38/food-1050813_1280.jpg"
-               },
-               {
-                  "idima": "img4",
-                  "img": "https://cdn.pixabay.com/photo/2021/03/16/10/04/street-6099209_1280.jpg"
-               }
-            ],
-            "contact": [
-               {
-                  "idCont": "contact1",
-                  "type": "telefono",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact2",
-                  "type": "whatsapp",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact3",
-                  "type": "facebook",
-                  "contact": "https://www.google.com"
-               },
-               {
-                  "idCont": "contact4",
-                  "type": "mensaje",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact5",
-                  "type": "link",
-                  "contact": "https://www.google.com"
+                  "idEstab": "estab1",
+                  "name": "Hotel",
+                  "desing": 1,
+                  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed ex tincidunt, condimentum .",
+                  "details": [
+                     {
+                        "ide": "hotel1",
+                        "tipo": "tipo_Habitacion",
+                        "costo": "100 MXN"
+                     },
+                     {
+                        "ide": "hotel2",
+                        "tipo": "tipo_Habitacion",
+                        "costo": "100 MXN"
+                     },
+                     {
+                        "ide": "hotel3",
+                        "tipo": "tipo_Habitacion",
+                        "costo": "100 MXN"
+                     },
+                     {
+                        "ide": "hotel4",
+                        "tipo": "tipo_Habitacion",
+                        "costo": "100 MXN"
+                     },
+                     {
+                        "ide": "hotel5",
+                        "tipo": "tipo_Habitacion",
+                        "costo": "100 MXN"
+                     }
+                  ],
+                  "source": [
+                     {
+                        "idima": "img1",
+                        "img": "https://resizer.otstatic.com/v2/photos/wide-huge/2/47967436.jpg"
+                     },
+                     {
+                        "idima": "img2",
+                        "img": "https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_640.jpg"
+                     },
+                     {
+                        "idima": "img3",
+                        "img": "https://cdn.pixabay.com/photo/2015/11/19/10/38/food-1050813_1280.jpg"
+                     },
+                     {
+                        "idima": "img4",
+                        "img": "https://cdn.pixabay.com/photo/2021/03/16/10/04/street-6099209_1280.jpg"
+                     }
+                  ],
+                  "contact": [
+                     {
+                        "idCont": "contact1",
+                        "type": "telefono",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact2",
+                        "type": "whatsapp",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact3",
+                        "type": "facebook",
+                        "contact": "https://www.google.com"
+                     },
+                     {
+                        "idCont": "contact4",
+                        "type": "mensaje",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact5",
+                        "type": "link",
+                        "contact": "https://www.google.com"
+                     }
+                  ]
                }
             ]
          }
@@ -107,82 +107,79 @@
             "loc": {
                "latitude": 19.493037,
                "longitude": -98.535733
-            }
-         }
-      ],
-      "info": [
-         {
-            "idEstab": "estab1",
-            "name": "Hotel",
-            "desing": 2,
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed ex tincidunt, condimentum .",
-            "details": [
+            },
+            "info": [
                {
-                  "ide": "rest1",
-                  "platillo": "Chinocuiles",
-                  "costo": "100 MXN"
-                },
-                {
-                  "ide": "rest2",
-                  "platillo": "Chinocuiles",
-                  "costo": "100 MXN"
-                },
-                {
-                  "ide": "rest3",
-                  "platillo": "Chinocuiles",
-                  "costo": "100 MXN"
-                }
-            ],
-            "source": [
-               {
-                  "idima": "img1",
-                  "img": "https://cdn.pixabay.com/photo/2015/11/19/10/38/food-1050813_1280.jpg"
-               },
-               {
-                  "idima": "img2",
-                  "img": "https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_640.jpg"
-               },
-               {
-                  "idima": "img3",
-                  "img": "https://resizer.otstatic.com/v2/photos/wide-huge/2/47967436.jpg"
-               },
-               {
-                  "idima": "img4",
-                  "img": "https://cdn.pixabay.com/photo/2021/03/16/10/04/street-6099209_1280.jpg"
-               }
-            ],
-            "contact": [
-               {
-                  "idCont": "contact1",
-                  "type": "telefono",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact2",
-                  "type": "whatsapp",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact3",
-                  "type": "facebook",
-                  "contact": "https://www.google.com"
-               },
-               {
-                  "idCont": "contact4",
-                  "type": "mensaje",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact5",
-                  "type": "link",
-                  "contact": "https://www.google.com"
+                  "idEstab": "estab1",
+                  "name": "Hotel",
+                  "desing": 2,
+                  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed ex tincidunt, condimentum .",
+                  "details": [
+                     {
+                        "ide": "rest1",
+                        "platillo": "Chinocuiles",
+                        "costo": "100 MXN"
+                      },
+                      {
+                        "ide": "rest2",
+                        "platillo": "Chinocuiles",
+                        "costo": "100 MXN"
+                      },
+                      {
+                        "ide": "rest3",
+                        "platillo": "Chinocuiles",
+                        "costo": "100 MXN"
+                      }
+                  ],
+                  "source": [
+                     {
+                        "idima": "img1",
+                        "img": "https://cdn.pixabay.com/photo/2015/11/19/10/38/food-1050813_1280.jpg"
+                     },
+                     {
+                        "idima": "img2",
+                        "img": "https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_640.jpg"
+                     },
+                     {
+                        "idima": "img3",
+                        "img": "https://resizer.otstatic.com/v2/photos/wide-huge/2/47967436.jpg"
+                     },
+                     {
+                        "idima": "img4",
+                        "img": "https://cdn.pixabay.com/photo/2021/03/16/10/04/street-6099209_1280.jpg"
+                     }
+                  ],
+                  "contact": [
+                     {
+                        "idCont": "contact1",
+                        "type": "telefono",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact2",
+                        "type": "whatsapp",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact3",
+                        "type": "facebook",
+                        "contact": "https://www.google.com"
+                     },
+                     {
+                        "idCont": "contact4",
+                        "type": "mensaje",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact5",
+                        "type": "link",
+                        "contact": "https://www.google.com"
+                     }
+                  ]
                }
             ]
          }
       ]
-      
-      
-      
    },
    {
       "idCat": "idCat3",
@@ -196,96 +193,93 @@
             "loc": {
                "latitude": 19.493037,
                "longitude": -98.535733
-            }
-         }
-      ],
-      "info":[
-         {
-            "idEstab": "estab1",
-            "name": "Avistamiento",
-            "desing": 3,
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed ex tincidunt, condimentum .",
-            "details": [
+            },
+            "info":[
                {
-                  "ide": "paq1",
-                  "nomPaquete": "Bronce",
-                  "costo": "100 MXN",
-                  "paquete": [
-                     "cosa 1",
-                     "cosa 2",
-                     "cosa 3"
+                  "idEstab": "estab1",
+                  "name": "Avistamiento",
+                  "desing": 3,
+                  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed ex tincidunt, condimentum .",
+                  "details": [
+                     {
+                        "ide": "paq1",
+                        "nomPaquete": "Bronce",
+                        "costo": "100 MXN",
+                        "paquete": [
+                           "cosa 1",
+                           "cosa 2",
+                           "cosa 3"
+                        ]
+                     },
+                     {
+                        "ide": "paq2",
+                        "nomPaquete": "Plata",
+                        "costo": "100 MXN",
+                        "paquete": [
+                           "cosa 1",
+                           "cosa 2",
+                           "cosa 3"
+                        ]
+                     },
+                     {
+                        "ide": "paq3",
+                        "nomPaquete": "ORO",
+                        "costo": "100 MXN",
+                        "paquete": [
+                           "cosa 1",
+                           "cosa 2",
+                           "cosa 3"
+                        ]
+                     }
+                  ],
+                  "source": [
+                     {
+                        "idima": "img1",
+                        "img": "https://cdn.pixabay.com/photo/2015/11/19/10/38/food-1050813_1280.jpg"
+                     },
+                     {
+                        "idima": "img2",
+                        "img": "https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_640.jpg"
+                     },
+                     {
+                        "idima": "img3",
+                        "img": "https://resizer.otstatic.com/v2/photos/wide-huge/2/47967436.jpg"
+                     },
+                     {
+                        "idima": "img4",
+                        "img": "https://cdn.pixabay.com/photo/2021/03/16/10/04/street-6099209_1280.jpg"
+                     }
+                  ],
+                  "contact": [
+                     {
+                        "idCont": "contact1",
+                        "type": "telefono",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact2",
+                        "type": "whatsapp",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact3",
+                        "type": "facebook",
+                        "contact": "https://www.google.com"
+                     },
+                     {
+                        "idCont": "contact4",
+                        "type": "mensaje",
+                        "contact": "7491086498"
+                     },
+                     {
+                        "idCont": "contact5",
+                        "type": "link",
+                        "contact": "https://www.google.com"
+                     }
                   ]
-               },
-               {
-                  "ide": "paq2",
-                  "nomPaquete": "Plata",
-                  "costo": "100 MXN",
-                  "paquete": [
-                     "cosa 1",
-                     "cosa 2",
-                     "cosa 3"
-                  ]
-               },
-               {
-                  "ide": "paq3",
-                  "nomPaquete": "ORO",
-                  "costo": "100 MXN",
-                  "paquete": [
-                     "cosa 1",
-                     "cosa 2",
-                     "cosa 3"
-                  ]
-               }
-            ],
-            "source": [
-               {
-                  "idima": "img1",
-                  "img": "https://cdn.pixabay.com/photo/2015/11/19/10/38/food-1050813_1280.jpg"
-               },
-               {
-                  "idima": "img2",
-                  "img": "https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_640.jpg"
-               },
-               {
-                  "idima": "img3",
-                  "img": "https://resizer.otstatic.com/v2/photos/wide-huge/2/47967436.jpg"
-               },
-               {
-                  "idima": "img4",
-                  "img": "https://cdn.pixabay.com/photo/2021/03/16/10/04/street-6099209_1280.jpg"
-               }
-            ],
-            "contact": [
-               {
-                  "idCont": "contact1",
-                  "type": "telefono",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact2",
-                  "type": "whatsapp",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact3",
-                  "type": "facebook",
-                  "contact": "https://www.google.com"
-               },
-               {
-                  "idCont": "contact4",
-                  "type": "mensaje",
-                  "contact": "7491086498"
-               },
-               {
-                  "idCont": "contact5",
-                  "type": "link",
-                  "contact": "https://www.google.com"
                }
             ]
          }
       ]
-      
-      
-      
    }
 ]
